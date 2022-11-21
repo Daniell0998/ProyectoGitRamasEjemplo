708577674c9c91dfc79440e531947789fd65e25f
usuario <usuario@DESKTOP-0MUMB30>
2022-11-21 19:58:45 +0100

    Hola mundo
    un Syso en el que se muestra Hola mundo

---
+++ EJED/src/EJ1.java
@@ -0,0 +1,14 @@
+
+public class EJ1 {
+
+	public static void main(String[] args) {
+		//Crea un proyecto público que se llame ProyectoGitRamasEjemplo.
+		//Añade un “hola mundo”, y subelo a GitHub.
+		
+		
+		
+		System.out.println("Hola mundo");
+
+	}
+
+}
