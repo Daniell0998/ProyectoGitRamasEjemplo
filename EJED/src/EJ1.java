2dd9167ab3709fd1a64123e0e72f7967d3d1dc2e
Daniel <118214444+Daniell0998@users.noreply.github.com>
2022-11-22 20:16:30 +0100

    Update EJ1.java

--- EJED/src/EJ1.java
+++ EJED/src/EJ1.java
@@ -5,10 +5,13 @@ public class EJ1 {
 		//Crea un proyecto público que se llame ProyectoGitRamasEjemplo.
 		//Añade un “hola mundo”, y subelo a GitHub.
 		
+		imprimesaludo();
 		
-		
-		System.out.println("Hola mundo");
+
 
 	}
+public static void imprimeSaludo() { 
+System.out.println(“Hei”);	
+}
 
 }
